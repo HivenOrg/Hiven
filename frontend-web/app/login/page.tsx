96ea11aa63a7daf7598c10b775aa78bad268e035
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-09-23 21:16:57 +0530

    completed login page

---
+++ frontend-web/app/login/page.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login to your account",
+};
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center  bg-background">
+      <div className="w-full max-w-lg p-5">
+        <h2 className="mb-6 text-center text-4xl font-bold text-primary">
+          Welcome back
+        </h2>
+        <form className="space-y-6">
+          <div>
+            <Label htmlFor="email" className="mb-2 block text-sm font-medium">
+              Email
+            </Label>
+            <Input id="email" placeholder="email" className="rounded-lg"/>
+          </div>
+          <div className="mb-1">
+            <Label htmlFor="password" className="mb-2 block text-sm font-medium">
+              Password
+            </Label>
+            <Input id="password" placeholder="password" type="password" className="rounded-lg" />
+          </div>
+          <div className="flex items-center justify-end">
+            <Button variant="link" className="text-sm">
+              Forgot password?
+            </Button>
+          </div>
+          <Button
+            type="submit"
+            className="w-full rounded-full"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}           
